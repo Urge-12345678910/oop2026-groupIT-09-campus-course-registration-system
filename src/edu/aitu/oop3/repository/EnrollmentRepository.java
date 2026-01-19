879acd74d10fd7bc1c5c397f7c5db3f87d7728e4
ehsan08-lmy <255860@astanait.edu.kz>
2026-01-19 17:20:23 +0500

    Ehsanullah Alimy commits enrollment and student repositories

--- src/edu/aitu/oop3/repository/EnrollmentRepository.java
+++ src/edu/aitu/oop3/repository/EnrollmentRepository.java
@@ -1,4 +1,15 @@
 package edu.aitu.oop3.repository;
 
+import edu.aitu.oop3.entity.Enrollment;
+import java.util.List;
+
 public interface EnrollmentRepository {
+
+    void enroll(int studentId, int courseId);
+
+    void remove(int studentId, int courseId);
+
+    List<Enrollment> findByStudentId(int studentId);
+
+    List<Enrollment> findByCourseId(int courseId);
 }
--- src/edu/aitu/oop3/repository/StudentRepository.java
+++ src/edu/aitu/oop3/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package edu.aitu.oop3.repository;
 
+import edu.aitu.oop3.entity.Student;
+import java.util.List;
+
 public interface StudentRepository {
+    Void save(Student student);
+    Student findById(int id);
+    List<Student> findAll();
 }
